e763f5cb32c0db00f9e9d96df4a073c8062ea70b
bilaljanir <bilaljanir@gmail.com>
2023-07-27 09:29:51 +0200

    Add button back-to-top

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,5 +30,12 @@
         });
     });
 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
 
 })(jQuery);
